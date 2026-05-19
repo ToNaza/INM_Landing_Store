26f93af8081fa6172540fab8d791bef479ce3834
ToNaza <misterket37@gmail.com>
2026-05-19 10:49:39 +0300

    fix 14

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
-const SUPABASE_URL = "https://yvujvjebfpllkyjswxra.supabase.co"; 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2dWp2amVicGZsbGt5anN3eHJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MjU4MDksImV4cCI6MjA2MzMwMTgwOX0.999-sb_publishable_Xbtp8BJos4Vmh22pcVuUMg_HQGuFS1s"; 
+const SUPABASE_URL = "https://yvujvjebfpllkyjswxra.supabase.co"; // Твой API URL со скрина
+const SUPABASE_KEY = "sb_publishable_Xbtp8BJos4Vmh22pcVuUMg_HQGuFS1s"; // Твой anon public со скрина
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
